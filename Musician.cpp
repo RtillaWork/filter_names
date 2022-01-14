56fc997671b2f1575b91f2f49ce029793c31708b
NNNE <39413995+RtillaWork@users.noreply.github.com>
2022-01-14 08:37:08 -0700

    add definitions to Musician class

--- Musician.cpp
+++ Musician.cpp
@@ -3,3 +3,80 @@
 //
 
 #include "Musician.h"
+
+Musician::Musician() {}
+
+
+Musician::Musician(const std::string &firstName, const std::string &lastName) : first_name_(firstName), middle_name_(),
+                                                                                last_name_(lastName) {}
+
+Musician::Musician(const std::string &firstName, const std::string &middleName, const std::string &lastName)
+        : first_name_(firstName), middle_name_(middleName), last_name_(lastName) {}
+
+bool Musician::operator==(const Musician &rhs) const {
+    return first_name_ == rhs.first_name_ &&
+           middle_name_ == rhs.middle_name_ &&
+           last_name_ == rhs.last_name_;
+}
+
+bool Musician::operator!=(const Musician &rhs) const {
+    return !(rhs == *this);
+}
+
+std::ostream &operator<<(std::ostream &os, const Musician &musician) {
+    os << "first_name_: " << musician.first_name_ << " middle_name_: " << musician.middle_name_ << " last_name_: "
+       << musician.last_name_;
+    return os;
+}
+
+bool Musician::operator<(const Musician &rhs) const {
+    if (first_name_ < rhs.first_name_)
+        return true;
+    if (rhs.first_name_ < first_name_)
+        return false;
+    if (middle_name_ < rhs.middle_name_)
+        return true;
+    if (rhs.middle_name_ < middle_name_)
+        return false;
+    return last_name_ < rhs.last_name_;
+}
+
+bool Musician::operator>(const Musician &rhs) const {
+    return rhs < *this;
+}
+
+bool Musician::operator<=(const Musician &rhs) const {
+    return !(rhs < *this);
+}
+
+bool Musician::operator>=(const Musician &rhs) const {
+    return !(*this < rhs);
+}
+
+Musician::~Musician() {
+
+}
+
+const std::string &Musician::getFirstName() const {
+    return first_name_;
+}
+
+void Musician::setFirstName(const std::string &firstName) {
+    first_name_ = firstName;
+}
+
+const std::string &Musician::getMiddleName() const {
+    return middle_name_;
+}
+
+void Musician::setMiddleName(const std::string &middleName) {
+    middle_name_ = middleName;
+}
+
+const std::string &Musician::getLastName() const {
+    return last_name_;
+}
+
+void Musician::setLastName(const std::string &lastName) {
+    last_name_ = lastName;
+}
--- Musician.h
+++ Musician.h
@@ -6,7 +6,49 @@
 #define FILTER_NAMES_MUSICIAN_H
 
 
+#include <string>
+#include <ostream>
+
 class Musician {
+private:
+    std::string first_name_;
+    std::string middle_name_;
+    std::string last_name_;
+
+public:
+    Musician();
+
+    Musician(const std::string &firstName, const std::string &middleName, const std::string &lastName);
+
+    Musician(const std::string &firstName, const std::string &lastName);
+
+    bool operator==(const Musician &rhs) const;
+
+    bool operator!=(const Musician &rhs) const;
+
+    friend std::ostream &operator<<(std::ostream &os, const Musician &musician);
+
+    bool operator<(const Musician &rhs) const;
+
+    bool operator>(const Musician &rhs) const;
+
+    bool operator<=(const Musician &rhs) const;
+
+    bool operator>=(const Musician &rhs) const;
+
+    virtual ~Musician();
+
+    const std::string &getFirstName() const;
+
+    void setFirstName(const std::string &firstName);
+
+    const std::string &getMiddleName() const;
+
+    void setMiddleName(const std::string &middleName);
+
+    const std::string &getLastName() const;
+
+    void setLastName(const std::string &lastName);
 
 };
 
